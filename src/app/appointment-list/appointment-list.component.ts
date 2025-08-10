123b81483e156b529d33938b445e7653bd9bac0a
pouya <pouyapouryaie@gmail.com>
2025-08-10 15:28:01 -0300

    using two-way binding and ng FormsModule to Add appointment creation functionality and update list

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -11,9 +11,30 @@ export class AppointmentListComponent {
   // define a property for class
   // appointment: string = 'take dog for a walk';
 
-  appointment: Appointment = {
-    id: 1,
-    title: 'take dog for a walk',
-    date: new Date('2025-08-10')
+  // appointment: Appointment = {
+  //   id: 1,
+  //   title: 'take dog for a walk',
+  //   date: new Date('2025-08-10')
+  // }
+
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+
+  appointments: Appointment[] = [];
+
+  addAppointment() {
+    
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: this.appointments.length + 1,
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+
+      this.appointments.push(newAppointment);
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+      alert(this.appointments.length);
+    }
   }
 }
